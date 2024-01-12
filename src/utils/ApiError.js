e7f11c5bb4106d215a60af1fd738b2ea8f394377
Ritesh <codeshorts007@gmail.com>
2024-01-12 17:02:53 +0530

    fix stack spelling in ApiError file

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -12,8 +12,8 @@ export class ApiError extends Error {
     this.success = false;
     this.errors = errors;
 
-    if (statck) {
-      this.stack = statck;
+    if (stack) {
+      this.stack = stack;
     } else {
       Error.captureStackTrace(this, this.constructor);
     }
