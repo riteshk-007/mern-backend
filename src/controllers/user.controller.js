7dbe5fc5ebbf1c449eb9ff05905e25f75d9876d2
riteshk_007 <codeshorts@gmail.com>
2024-01-15 16:37:03 +0530

    Add user avatar update functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -265,3 +265,30 @@ export const currentUser = AsyncHandler(async(req, res)=>{
     .status(200)
     .json(new ApiResponse(200, user, "user profile updated successfully"));
  })
+
+ // update user avatar
+ export const updateAvatar = AsyncHandler(async(req, res)=>{
+    const avatarLocalPath = req.file?.path;
+    if (!avatarLocalPath) {
+      throw new ApiError(400, "Avatar is required");
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    if (!avatar.url) {
+      throw new ApiError(500, "Error while uploading on avatar");
+    }
+
+    const user = await User.findByIdAndUpdate(req.user._id, {
+       
+        $set: {
+          avatar: avatar.url,
+        }
+    
+    }, {
+      new: true,
+    }).select("-password");
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "user avatar updated successfully"));
+ })
