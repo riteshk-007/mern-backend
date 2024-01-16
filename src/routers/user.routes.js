7dcb032ceefbad58634d8e18d1ab57a6d74e5696
riteshk_007 <codeshorts@gmail.com>
2024-01-16 16:18:48 +0530

    Add new routes and controllers for user profile and account management

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { 
+    GetWatchHistory,
+    changeCurrentPassword, 
+    currentUser,
+    getUserChannelProfile,
+    loginUser,
+    logoutUser,
+    refreshAccessToken, 
+    registerUser, 
+    updateAvatar,
+    updateCoverImage,
+    updateProfile 
+        } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 
@@ -24,4 +36,15 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyToken, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyToken, changeCurrentPassword)
+router.route('/current-user').get(verifyToken, currentUser)
+router.route("/update-account").patch(verifyToken, updateProfile)
+
+router.route("/update-avatar").patch(verifyToken, upload.single("avatar"), updateAvatar)
+router.route("/cover-image").patch(verifyToken, upload.single("coverImage"),updateCoverImage)
+
+router.route("/c/:username").get(verifyToken, getUserChannelProfile)
+
+router.route("/history").get(verifyToken, GetWatchHistory)
+
 export default router;
